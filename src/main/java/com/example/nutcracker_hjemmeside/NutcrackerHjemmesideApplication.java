08d89362a32480f1b4cfa8a17f7f0c7eaafcf09d
danieljappe <danieljappe@icloud.com>
2023-02-08 16:31:47 +0100

    start

---
+++ src/main/java/com/example/nutcracker_hjemmeside/NutcrackerHjemmesideApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutcracker_hjemmeside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutcrackerHjemmesideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutcrackerHjemmesideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nutcracker_hjemmeside/NutcrackerHjemmesideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutcracker_hjemmeside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutcrackerHjemmesideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
